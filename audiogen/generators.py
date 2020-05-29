fc67e96e7a6a9e2147f55c94c17fc6bc0c698101
gdusbabek <gdusbabek@gmail.com>
2020-05-29 15:09:44 -0500

    split out embedded functions for tone generation

--- audiogen/generators.py
+++ audiogen/generators.py
@@ -1,7 +1,7 @@
 # coding=utf8
 
 '''
-Assorted generators 
+Assorted generators
 '''
 
 import math
@@ -18,31 +18,32 @@ def beep(frequency=440, seconds=0.25):
 	for sample in util.crop_with_fades(tone(frequency), seconds=seconds):
 		yield sample
 
+def _fixed_tone(frequency):
+	period = int(sampler.FRAME_RATE / frequency)
+	time_scale = 2 * math.pi / period # period * scale = 2 * pi
+	# precompute fixed tone samples # TODO: what about phase glitches at end?
+	samples = [math.sin(i * time_scale) for i in xrange(period)]
+	while True:
+		for i in xrange(period):
+			yield samples[i]
+
+def _variable_tone(frequency):
+	time_scale = TWO_PI / sampler.FRAME_RATE
+	phase = 0
+	for f in frequency:
+		yield math.sin(phase)
+
+		phase += time_scale * f
+
+		# don't reset hard to zero – avoids sudden phase glitches due to rounding error
+		if phase > TWO_PI:
+			phase -= TWO_PI
+
 def tone(frequency=440, min_=-1, max_=1):
-	def fixed_tone(frequency):
-		period = int(sampler.FRAME_RATE / frequency) 
-		time_scale = 2 * math.pi / period # period * scale = 2 * pi
-		# precompute fixed tone samples # TODO: what about phase glitches at end?
-		samples = [math.sin(i * time_scale) for i in xrange(period)]
-		while True:
-			for i in xrange(period):
-				yield samples[i]
-	def variable_tone(frequency):
-		time_scale = TWO_PI / sampler.FRAME_RATE
-		phase = 0
-		for f in frequency:
-			yield math.sin(phase)
-
-			phase += time_scale * f
-
-			# don't reset hard to zero – avoids sudden phase glitches due to rounding error
-			if phase > TWO_PI:
-				phase -= TWO_PI
-
 	if not hasattr(frequency, 'next'):
-		gen = fixed_tone(frequency)
+		gen = _fixed_tone(frequency)
 	else:
-		gen = variable_tone(frequency)
+		gen = _variable_tone(frequency)
 	return util.normalize(gen, -1, 1, min_, max_)
 
 def synth(freq, angles):
